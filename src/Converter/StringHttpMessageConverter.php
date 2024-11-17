3b118b1733c25075408674e82f1e4aeb10de7407
xgbnl <745502274@qq.com>
2024-11-17 09:04:31 +0800

    chore: 优化代码

--- src/Converter/StringHttpMessageConverter.php
+++ src/Converter/StringHttpMessageConverter.php
@@ -12,20 +12,6 @@ final readonly class StringHttpMessageConverter extends AbstractHttpMessageConve
   #[Override]
   protected function write(Response $body): Responsable
   {
-    $content = !$this->isChinese($body) || $this->isBase64($body)
-      ? new AnonymousValue($body->getContent())
-      : $body->getContent();
-
-    return $this->factory->toResponse($content);
-  }
-
-  public function isChinese(Response $response): bool
-  {
-    return preg_match('/[\x{4e00}-\x{9fa5}]/u', $response->getContent()) > 0;
-  }
-
-  public function isBase64(Response $response): bool
-  {
-    return preg_match('/^[A-Za-z0-9+\/=]+$/', $response->getContent()) && strlen($response->getContent()) % 4 === 0;
+    return $this->factory->toResponse($body->getContent());
   }
 }
